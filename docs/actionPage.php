5d4914db9996ac614a684c742ebb2f7be541f654
susancolby <78624571+susancolby@users.noreply.github.com>
2022-06-08 14:56:34 -0500

    Update and rename actionPage.html to actionPage.php

---
+++ docs/actionPage.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+  <title>Articles</title>
+<style>
+  h2 {
+  font-family:'Yeseva One';
+  font-size:30px
+  } 
+</style>
+  
+  <h2>Welcome <?php echo $_POST["name"]; ?><h2><br>
+
+</body>
+</html>
